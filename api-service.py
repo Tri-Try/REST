7ff75dc150ef3c648879ef805fe7acb8367e58c5
Salas <gae.m.project@gmail.com>
2015-12-03 23:29:31 +0800

    remove jokes on MIS :jack_o_lantern::sake::tea::beers:

--- api-service.py
+++ api-service.py
@@ -10,10 +10,6 @@ def index_service():
     return render_template('index.html')
 
 
-@app.route("/admin")
-def gggg():
-    return '不是很邱嗎? Jesse不就好棒棒，不要亂玩server'
-
 app.register_blueprint(
     api_v1_bp,
     url_prefix='/{prefix}/{version}'.format(prefix='api', version=API_VERSION_V1)
