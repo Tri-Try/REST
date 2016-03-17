b283a6f78a089c428d9ccca1fda3caf6121f34b4
Vagrant Default User <vagrant@debian-jessie.vagrantup.com>
2016-03-17 13:57:16 +0000

    Modify docker-compose config for newest Docker

--- app.py
+++ app.py
@@ -2,15 +2,14 @@ import os
 from application.api_service import create_app
 from application.models import db
 
+app = create_app()
+
 
 def setup_database(app):
     with app.app_context():
         db.create_all()
 
-
 if __name__ == '__main__':
-
-    app = create_app()
 
     if not os.path.isfile('../sqlite3.db'):
         setup_database(app)
