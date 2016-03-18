de68cb13f13b1cb6a78791ba5e850f8274bd9bda
salas <gae.m.project@gmail.com>
2016-03-18 05:26:52 -0400

    move commands into manage.py script

--- application/page.py
+++ application/page.py
@@ -1,7 +1,6 @@
 from flask import render_template, send_file
 from flask.blueprints import Blueprint
 
-from nthu_library import NTHULibrary
 from application.api.v1 import docs as v1_docs
 
 page_bp = Blueprint('page', __name__)
@@ -18,33 +17,3 @@ def doc_service(path):
         path = 'index.html'
     return send_file('static/doc/%s' % path)
 
-
-@page_bp.route("/update_sheets")
-def crawl_sheets_service():
-    lib = NTHULibrary()
-    NTHULibrary.get_past_year_questions()
-    lib.get_top_circulated_materials(type='loaned')
-    lib.get_top_circulated_materials(type='reserved')
-    return '抓抓抓抓抓完啦'
-
-
-@page_bp.route('/map')
-def list_routes():
-    import urllib
-    output = []
-    from flask import url_for,  current_app as app
-    for rule in app.url_map.iter_rules():
-
-        options = {}
-        for arg in rule.arguments:
-            options[arg] = "[{0}]".format(arg)
-
-        methods = ','.join(rule.methods)
-        url = url_for(rule.endpoint, **options)
-        line = ("{:50s} {:20s} {}".format(rule.endpoint, methods, url))
-        output.append(line)
-
-    for line in sorted(output):
-        print(line)
-
-    return '<br>'.join(output)
--- manage.py
+++ manage.py
@@ -1,18 +1,47 @@
 from flask.ext.script import Command, Manager
+
 from application.api_service import create_app
+from application.models import db
+
+from nthu_library import NTHULibrary
 
 
 app = create_app()
 manager = Manager(app)
 
 
-class Hello(Command):
-    "prints hello world"
+@manager.command
+def syncdb():
+    with app.app_context():
+        db.create_all()
 
-    def run(self):
-        print("apidoc --input application/ --output application/static/doc/")
 
-manager.add_command('hello', Hello())
+@manager.command
+def crawl():
+    lib = NTHULibrary()
+    NTHULibrary.get_past_year_questions()
+    lib.get_top_circulated_materials(type='loaned')
+    lib.get_top_circulated_materials(type='reserved')
+
+
+@manager.command
+def routes():
+    import urllib
+    output = []
+    from flask import url_for, current_app as app
+    for rule in app.url_map.iter_rules():
+
+        options = {}
+        for arg in rule.arguments:
+            options[arg] = "[{0}]".format(arg)
+
+        methods = ','.join(rule.methods)
+        url = url_for(rule.endpoint, **options)
+        line = ("{:50s} {:20s} {}".format(rule.endpoint, methods, url))
+        output.append(line)
+
+    for line in sorted(output):
+        print(line)
 
 
 if __name__ == "__main__":
