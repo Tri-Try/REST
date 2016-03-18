76ad8c05075188e16f18728d391cd3ce1a2e8d92
Salas <gae.m.project@gmail.com>
2016-03-18 08:11:09 +0000

    generated static documents for API v2 :tea:

---
+++ application/static/doc/api_project.js
@@ -0,0 +1,15 @@
+define({
+  "name": "Open Campus: Open data REST API",
+  "version": "2.0.0",
+  "description": "This is an API system provided by Tritry@github",
+  "title": "Open Campus - Open Data REST API",
+  "url": "http://tritry.cs.nthu.edu.com/api/v2",
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2016-03-18T07:36:09.103Z",
+    "url": "http://apidocjs.com",
+    "version": "0.15.1"
+  }
+});
